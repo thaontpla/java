4f97ca7c7d16b4cdb18855bfe3ce80e8213ae526
thaontpla <thaontp.la@gmail.com>
2023-06-04 20:33:38 +0700

    bai4

--- src/main/java/com/day03/bai4.java
+++ src/main/java/com/day03/bai4.java
@@ -3,8 +3,8 @@ package com.day03;
 import java.util.Scanner;
 
 //Viết chương trình nhập hai số nguyên n, m
-// và in ra hình chữ nhật đặc các dấu * kích thước n*m. Ví dụ với n=5, m=4:
-public class Bai3 {
+// và in ra hình chữ nhật rỗng các dấu * kích thước n*m. Ví dụ với n=5, m=4:
+public class bai4 {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         int n, m;
@@ -14,9 +14,13 @@ public class Bai3 {
         m = input.nextInt();
         for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= m; j++) {
-                System.out.print("* ");
+                if (i == 1 || i == n || j == 1 || j == m) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
             }
-            System.out.println("* ");
+            System.out.println();
         }
     }
 }
