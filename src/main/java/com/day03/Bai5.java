3ca95196bfa90aaf52c3a28b8d52135edc0f23ea
thaontpla <thaontp.la@gmail.com>
2023-06-04 20:34:37 +0700

    bai5

---
+++ src/main/java/com/day03/Bai5.java
@@ -0,0 +1,11 @@
+package com.day03;
+//Viết một chương trình Java để đảo ngược một số nguyên dương nhập từ bàn phím
+// bằng cách sử dụng vòng lặp while và do-while và break và continue.
+// Chương trình sẽ yêu cầu người dùng nhập một số nguyên dương,
+// sau đó đảo ngược và in ra số đã đảo ngược.
+//Ví dụ: Nhập vào số 123456 --> Kết quả nhận được là số: 654321
+public class Bai5 {
+    public static void main(String[] args) {
+
+    }
+}
