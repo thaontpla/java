86a9ea7eba8ce1c8869fe33fad036e6fdf2d8464
thaontpla <thaontp.la@gmail.com>
2023-06-07 19:21:08 +0700

    fix

--- src/main/java/day04/Bai2BTVN.java
+++ src/main/java/day04/Bai2BTVN.java
@@ -8,7 +8,7 @@ public class Bai2BTVN {
         String[] array1 = {"Python", "JAVA", "PHP", "C#", "C++", "SQL"};
         String[] array2 = {"MySQL", "SQL", "SQLite", "Oracle", "PostgreSQL", "DB2", "JAVA"};
         for (int i = 0; i < array1.length; i++) {
-            for (int j = 0; j < array2.length; i++) {
+            for (int j = 0; j < array2.length; j++) {
                 if (array1[i].equals(array2[j]) ) {
                     System.out.println(array1[i]);
                 }
