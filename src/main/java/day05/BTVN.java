4aed46f2f5bdd39ed14efa2d9c66daf719fd8177
thaontpla <75661676+thaontpla@users.noreply.github.com>
2023-06-08 18:02:40 +0700

    BT

---
+++ src/main/java/day05/BTVN.java
@@ -0,0 +1,14 @@
+package day05;
+//Bài 1: Viết chương trình liệt kê các số nguyên có 7 đến 9 chữ số thoả mãn:
+//a) Là số thuận nghịch.
+//b) Chỉ có chữ số 0, 6, 8
+//c) Tổng chữ số chia hết cho 10
+public class BTVN {
+    public static void main(String[] args) {
+        bai1();
+    }
+
+    static void bai1() {
+
+    }
+}
