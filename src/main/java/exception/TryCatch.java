26a0d767d7f2e27790dea25b1c457e1e780ad7af
thaontpla <75661676+thaontpla@users.noreply.github.com>
2023-06-28 18:18:33 +0700

    update

--- src/main/java/exception/TryCatch.java
+++ src/main/java/exception/TryCatch.java
@@ -1,25 +1,28 @@
 package exception;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class TryCatch {
     public static void main(String[] args) {
-        thuonghaiso();
+        //thuonghaiso();
+        thuonghaisover2();
     }
 
-    private static void thuonghaiso() {
+    private static void thuonghaiso() { //nếu ko muốn nhập nhiều thì cho theem count.
         int a = nhapso("Nhap a: ");
         int b = nhapso("Nhap b: ");
         try {
             do {
-                System.out.println("a/b là: " + a/b);
+                System.out.println("a/b là: " + a / b);
                 break;
-            }while (true);
-        }catch (Exception e){
+            } while (true);
+        } catch (Exception e) {
             //xử lý khi có ngoại lệ xẩy ra
             System.out.println("Nhập không đúng định dạng lắm");
         }
     }
+
     private static int nhapso(String message) {
         int x = 0;
         try {
@@ -27,11 +30,28 @@ public class TryCatch {
                 System.out.println(message);
                 x = new Scanner(System.in).nextInt();
                 break;
-            }while (true);
-        }catch (Exception e){
+            } while (true);
+        } catch (Exception e) {
             //xử lý khi có ngoại lệ xẩy ra
             System.out.println("Nhập không đúng định dạng");
         }
         return x;
+    }
+
+    private static void thuonghaisover2() {
+        try {
+            System.out.println("Nhap a: ");
+            int a = new Scanner(System.in).nextInt();
+            System.out.println("Nhap b: ");
+            int b = new Scanner(System.in).nextInt();
+            System.out.println("a/b = " + a / b);
+
+        } catch (InputMismatchException e) {
+            //e.printStackTrace();
+            //xử lý khi có ngoại lệ xẩy ra
+            System.out.println("Nhập không đúng định dạng lắm");
+        } catch (ArithmeticException e) {
+            System.out.println("KO thể chia cho 0");
+        }
     }
 }
---
+++ src/main/java/file/Demo.java
@@ -0,0 +1,7 @@
+package file;
+
+public class Demo {
+    public static void main(String[] args) {
+
+    }
+}
